ecad4e6255e044ca66d72c85697aa7193fd8b718
GitLukeW <gitlukew@gmail.com>
2022-09-13 17:05:30 -0400

    built the models

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Book, Tracking, Note
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 
 admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Book)
+admin.site.register(Tracking)
+admin.site.register(Note)
+
 
--- core/models.py
+++ core/models.py
@@ -1,5 +1,8 @@
+from secrets import choice
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models.constraints import UniqueConstraint
 
 
 class CustomUser(AbstractUser):
@@ -7,3 +10,54 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    publication_date = models.DateField(blank=True, null=True)
+    genre = models.CharField(max_length=250)
+    featured = models.BooleanField(default=False)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['title', 'author'],
+                             name='unique_constraint')
+        ]
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Tracking(models.Model):
+    READING = 'RG'
+    READ = 'RD'
+    UNREAD = 'UR'
+    STATUS_CHOICES = [
+        (READING, 'reading'),
+        (READ, 'read'),
+        (UNREAD, 'unread'),
+    ]
+    status = models.CharField(
+        max_length=2, choices=STATUS_CHOICES, default=UNREAD)
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='tracking_users')
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name='tracking_books')
+
+    def __str__(self):
+        return f'{self.status}'
+
+
+class Note(models.Model):
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='note_users')
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name='note_books')
+    date_created = models.DateTimeField(auto_now_add=True)
+    notes = models.TextField(max_length=250)
+    privacy = models.BooleanField(default=True)
+    page_number = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.notes}'
